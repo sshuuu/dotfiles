bbf5032025841c9152ea3144c5dd9f25fac002b9
shuya sato <ssato@appirits.com>
2021-01-15 19:40:23 +0900

    aliasなど整理、README修正

--- README.md
+++ README.md
@@ -5,12 +5,11 @@ zshの設定ファイル用ディレクトリを変更するために、`/etc/zp
 ```bash
 export ZDOTDIR=$HOME/.zsh
 ```
+
+`/etc/zprofile`に環境変数を追記したら下記を実行してください。
+```bash
+source path/to/dotfiles/setup.sh
+```
+
 #### 参照
 - [zshの設定ファイルの読み込み順序と使い方Tipsまとめ](https://qiita.com/muran001/items/7b104d33f5ea3f75353f)
-
-### prezto
-~/.zpreztorcを編集して設定してください。
-
-#### 参照
-- [pureの設定](https://github.com/sindresorhus/pure)
-- [お前らのターミナルはダサい](https://qiita.com/kinchiki/items/57e9391128d07819c321)
--- setup.sh
+++ setup.sh
@@ -2,11 +2,21 @@
 if [ -n "$ZDOTDIR" ]; then
     mkdir -p $ZDOTDIR 2>/dev/null
 fi
-ln -sf ~/dotfiles/vimrc        $HOME/.vimrc
-ln -sf ~/dotfiles/zsh/zprofile ${ZDOTDIR:-$HOME}/.zprofile
-ln -sf ~/dotfiles/zsh/zshrc    ${ZDOTDIR:-$HOME}/.zshrc
 
-# install vim-plug
+############################################
+# zprofile, zshrc
+############################################
+if [ ! -e ${ZDOTDIR:-$HOME}/.zprofile ]; then
+    echo "source $HOME/dotfiles/zsh/zprofile" > ${ZDOTDIR:-$HOME}/.zprofile
+fi
+if [ ! -e ${ZDOTDIR:-$HOME}/.zshrc ]; then
+    echo "source $HOME/dotfiles/zsh/zshrc" > ${ZDOTDIR:-$HOME}/.zshrc
+fi
+
+############################################
+# vim
+############################################
+ln -sf ~/dotfiles/vimrc        $HOME/.vimrc
 if [ ! -e $HOME/.vim/autoload/plug.vim ]; then
     curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
