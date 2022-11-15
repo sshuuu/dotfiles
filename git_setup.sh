22a3dbe34700309be3241d065ee2b92a721f2032
Shuya Sato <shuyasato@ShuyanoMacBook-Pro.local>
2022-11-15 17:08:32 +0900

    git_setup.sh作成

---
+++ git_setup.sh
@@ -0,0 +1,9 @@
+git config --global alias.st status
+git config --global alias.co checkout
+git config --global alias.cob "checkout -b"
+git config --global alias.br branch
+# 綺麗なログ
+git config --global alias.logg "log --graph --pretty=format:'%x09%C(auto) %h %Cgreen %ar %Creset%x09by\"%C(cyan ul)%an%Creset\" %x09%C(auto)%s %d'"
+# スカッシュ（squash）
+git config --global alias.sq "rebase -i"
+
