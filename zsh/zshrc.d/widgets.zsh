f44deebdb171010aa75ca40e36faa64932a7bb7c
sshuuu <sshuuu.ne@gmail.com>
2025-07-27 03:41:09 +0900

    widgets修正

--- zsh/zshrc.d/widgets.zsh
+++ zsh/zshrc.d/widgets.zsh
@@ -1,19 +1,25 @@
 # ghq + fzf + zshウィジェット
 function fzf-ghq-widget() {
-  local selected_dir
-  selected_dir=$(ghq list -p | fzf --preview 'tree -L 1 {}' --height 40%)
-  if [ -n "$selected_dir" ]; then
-    cd "$selected_dir"
-    zle reset-prompt  # プロンプトを更新
+  local dir
+  dir=$(ghq list -p | fzf --preview 'tree -L 1 {}' --height 40%) || return
+  if [[ -n "$dir" && -d "$dir" ]]; then
+    BUFFER="cd \"$dir\""
+    zle accept-line   # コマンドラインを確定して実行
   fi
 }
 zle -N fzf-ghq-widget  # Zsh Line Editor 関数として登録
-bindkey '^g' fzf-ghq-widget  # Ctrl+g で実行
+bindkey '^G' fzf-ghq-widget
 
 # Gitブランチ選択ウィジェット
-fzf-git-checkout-widget() {
+function fzf-git-checkout-widget() {
   local branch
-  branch=$(git branch --all | grep -v HEAD | sed 's/^..//' | fzf) && git checkout "$branch"
+  branch=$(git branch --all | grep -v HEAD | sed 's/^..//' | fzf --height 40%) || return
+  if [[ -n "$branch" ]]; then
+    # remotes/origin/ を削除
+    clean_branch=${branch#remotes/origin/}
+    BUFFER="git checkout \"$clean_branch\""
+    zle accept-line  # コマンドラインを確定して実行
+  fi
 }
 zle -N fzf-git-checkout-widget
 bindkey '^B' fzf-git-checkout-widget
